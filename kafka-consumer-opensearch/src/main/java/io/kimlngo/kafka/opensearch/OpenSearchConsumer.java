b9d8fd565bd82623e9db4b6f0ae199fbfdc8739b
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-11-29 11:19:14 -0500

    Create OpenSearchConsumer

---
+++ kafka-consumer-opensearch/src/main/java/io/kimlngo/kafka/opensearch/OpenSearchConsumer.java
@@ -0,0 +1,128 @@
+package io.kimlngo.kafka.opensearch;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.CreateIndexRequest;
+import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class OpenSearchConsumer {
+    private static final Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
+    private static final String INDEXES = "wikimedia";
+
+    public static void main(String[] args) {
+        //create OpenSearch client
+        RestHighLevelClient openSearchClient = createOpenSearchClient();
+
+        //create Kafka Consumer
+        KafkaConsumer<String, String> kafkaConsumer = createKafkaConsumer();
+
+        try (openSearchClient; kafkaConsumer) {
+            boolean isExist = openSearchClient.indices()
+                                              .exists(new GetIndexRequest(INDEXES), RequestOptions.DEFAULT);
+
+            if (!isExist) {
+                CreateIndexRequest request = new CreateIndexRequest(INDEXES);
+                openSearchClient.indices()
+                                .create(request, RequestOptions.DEFAULT);
+
+                log.info("The wikimedia index has been created");
+            } else {
+                log.info("The wikimedia is already existed");
+            }
+
+            kafkaConsumer.subscribe(Collections.singleton("wikimedia.recentchange"));
+
+            while(true) {
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(3000));
+
+                int recordCount = records.count();
+                log.info("Received " + recordCount + " record(s)");
+
+                for(var record : records) {
+                    try {
+                        IndexRequest indexRequest = new IndexRequest(INDEXES).source(record.value(), XContentType.JSON);
+
+                        IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+
+                        log.info(response.getId());
+                    } catch (Exception e) {
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    private static KafkaConsumer<String, String> createKafkaConsumer() {
+        String groupId = "consumer-opensearch-demo";
+
+        //create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+
+        //config serializer
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+        //create consumer
+        return new KafkaConsumer<>(properties);
+    }
+
+    public static RestHighLevelClient createOpenSearchClient() {
+        String connString = "http://localhost:9200";
+
+        // we build a URI from the connection string
+        RestHighLevelClient restHighLevelClient;
+        URI connUri = URI.create(connString);
+        // extract login information if it exists
+        String userInfo = connUri.getUserInfo();
+
+        if (userInfo == null) {
+            // REST client without security
+            restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), "http")));
+
+        } else {
+            // REST client with security
+            String[] auth = userInfo.split(":");
+
+            CredentialsProvider cp = new BasicCredentialsProvider();
+            cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
+
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme()))
+                              .setHttpClientConfigCallback(
+                                      httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(cp)
+                                                                                      .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())));
+
+
+        }
+
+        return restHighLevelClient;
+    }
+}
--- kafka-producer-wikimedia/src/main/java/com/kimlngo/demos/kafka/wikimedia/WikimediaChangeHandler.java
+++ kafka-producer-wikimedia/src/main/java/com/kimlngo/demos/kafka/wikimedia/WikimediaChangeHandler.java
@@ -30,6 +30,7 @@ public class WikimediaChangeHandler implements EventHandler {
 
     @Override
     public void onMessage(String s, MessageEvent messageEvent) throws Exception {
+        log.info(messageEvent.getData());
         //asynchronous
         kafkaProducer.send(new ProducerRecord<>(this.topic, messageEvent.getData()));
     }
