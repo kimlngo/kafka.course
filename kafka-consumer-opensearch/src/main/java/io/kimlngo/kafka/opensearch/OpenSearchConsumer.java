003fbca9000d2693aef53aad55e858a9c868fda6
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-11-29 15:07:07 -0500

    Implement Graceful Shutdown for OpenSearchConsumer

--- kafka-consumer-opensearch/src/main/java/io/kimlngo/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/kimlngo/kafka/opensearch/OpenSearchConsumer.java
@@ -10,6 +10,7 @@ import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.opensearch.action.bulk.BulkRequest;
 import org.opensearch.action.bulk.BulkResponse;
@@ -24,6 +25,7 @@ import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.URI;
 import java.time.Duration;
 import java.util.Collections;
@@ -34,12 +36,28 @@ public class OpenSearchConsumer {
     private static final String INDEXES = "wikimedia";
     private static final String GROUP_ID = "consumer-opensearch-demo";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //create OpenSearch client
         RestHighLevelClient openSearchClient = createOpenSearchClient();
 
         //create Kafka Consumer
         KafkaConsumer<String, String> kafkaConsumer = createKafkaConsumer();
+
+        //get a reference to main thread
+        final Thread mainThread = Thread.currentThread();
+
+        //add a shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("Detected a shutdown, let's exit by calling consumer.wakeup() ...");
+            kafkaConsumer.wakeup();
+
+            //join the main thread to allow execution of the code in the main thread
+            try {
+                mainThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }));
 
         try (openSearchClient; kafkaConsumer) {
             boolean isExist = openSearchClient.indices()
@@ -89,11 +107,17 @@ public class OpenSearchConsumer {
                     Thread.sleep(1000);
 
                     kafkaConsumer.commitSync();
-                    log.info("Offset commited");
+                    log.info("Offset committed");
                 }
             }
+        } catch (WakeupException e) {
+            log.info("Consumer is starting to shutdown");
         } catch (Exception e) {
-
+            log.error("Unexpected exception in the consumer", e);
+        } finally {
+            kafkaConsumer.close();
+            openSearchClient.close();
+            log.info("The consumer is now gracefully shutdown ...");
         }
     }
 
